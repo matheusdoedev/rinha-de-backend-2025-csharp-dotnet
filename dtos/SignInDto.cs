0205b3737851bb8f878473e782abbf88ff288b95
Matheus do É Santos <58111490+matheusdoedev@users.noreply.github.com>
2025-08-15 16:19:55 +0000

    refactor: :construction: removes unnecessary files

--- dtos/SignInDto.cs
+++
@@ -1,7 +0,0 @@
-namespace AspNetWebApiBoilerplate.dtos;
-
-public class SignInDto
-{
-	public string UserName { get; set; } = "";
-	public string Password { get; set; } = "";
-}
--- dtos/SignUpResponseDto.cs
+++
@@ -1,4 +0,0 @@
-namespace AspNetWebApiBoilerplate.dtos;
-
-public class SignUpResponseDto : SignInResponseDto
-{}
