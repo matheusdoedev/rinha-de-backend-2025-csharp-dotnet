832b1e64f744320bb27739b9db903c4d5d0869be
matheusdoedev <matheusdoe.dev@gmail.com>
2025-08-16 17:08:41 -0300

    feat: :sparkles: adds payment foreign key to payment processing model

--- Domains/Payment/PaymentProcessing.cs
+++ Domains/Payment/PaymentProcessing.cs
@@ -9,5 +9,9 @@ public class PaymentProcessing
 	public string Id { get; set; } = Guid.NewGuid().ToString();
 	public ProcessingMethod ProcessingMethod { get; set; } = ProcessingMethod.STANDARD;
 	public PaymentProcessingStatus Status { get; set; } = PaymentProcessingStatus.IN_PROGRESS;
+
+	[ForeignKey("Payment")]
+	public string PaymentId { get; set; } = "";
+	public virtual Payment Payment { get; set; } = new();
 }
 
