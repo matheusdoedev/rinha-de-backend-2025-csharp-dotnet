014714df374c5ec9933d2c7f02a9edf53e1ebd2a
matheusdoedev <matheusdoe.dev@gmail.com>
2025-08-16 17:07:18 -0300

    feat: :wrench: configures nlog

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 using PaymentBroker.Contexts;
 
+using NLog;
 using DotEnv.Core;
+
+using PaymentBroker.Domains.Payment.Services;
+using PaymentBroker.Domains.Payment.Repositories;
 
 new EnvLoader().Load();
 
+Logger logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
 string connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION") ?? throw new ArgumentException("db connection string env not defined");
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +16,28 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
 builder.Services.AddAuthorization();
+builder.Logging.ClearProviders();
+builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Warning);
 
-WebApplication app = builder.Build();
+try
+{
+	WebApplication app = builder.Build();
 
-app.UseSwagger();
-app.UseSwaggerUI();
-app.MapControllers();
+	app.UseSwagger();
+	app.UseSwaggerUI();
+	app.MapControllers();
 
-await app.RunAsync();
+	await app.RunAsync();
+}
+catch (Exception exception)
+{
+	logger.Error(exception, "Application stopped due to an exception");
+	throw;
+}
+finally
+{
+	LogManager.Shutdown();
+}
