f255a6bad8b291779b131908ae1ddadd082c767a
matheusdoedev <matheusdoe.dev@gmail.com>
2025-08-16 17:09:27 -0300

    feat: :sparkles: creates payment repository

---
+++ Domains/Payment/Repositories/Impl/PaymentRepository.cs
@@ -0,0 +1,38 @@
+namespace PaymentBroker.Domains.Payment.Repositories;
+
+using Microsoft.EntityFrameworkCore;
+using PaymentBroker.Contexts;
+
+public class PaymentRepository(AppDbContext context) : IPaymentRepository
+{
+	private readonly AppDbContext _context = context;
+
+	public async Task Add(Payment payment)
+	{
+		await _context.AddAsync(payment);
+	}
+
+	public async Task<List<Payment>> GetAll()
+	{
+		List<Payment> payments = await _context.Payments.ToListAsync();
+
+		return payments;
+	}
+
+	public async Task<Payment> GetById(string PaymentId)
+	{
+		Payment payment = await _context.Payments.Where(p => p.Id == PaymentId).FirstAsync();
+
+		return payment;
+	}
+
+	public async Task Save()
+	{
+		await _context.SaveChangesAsync();
+	}
+
+	public DbSet<Payment> GetDbSet()
+	{
+		throw new NotImplementedException();
+	}
+}
